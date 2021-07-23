6c7144ee39098a4678fbbc5f4d045ed8939a1db8
Jorge Ramos <jorginhodeveloper@gmail.com>
2021-07-23 18:14:44 -0300

    feat: configure global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from "next/head";
 
-import styles from "../styles/home.module.scss";
-
 export default function Home() {
   return (
     <>
@@ -9,7 +7,7 @@ export default function Home() {
         <title>Início | ig.news</title>
       </Head>
 
-      <h1 className={styles.title}>Hello Next</h1>
+      <h1>Hello Next</h1>
     </>
   );
 }
