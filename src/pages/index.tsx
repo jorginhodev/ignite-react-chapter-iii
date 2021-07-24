d69f8774d5768dbea136fcb10f0ae1a02d6b4324
Jorge Ramos <jorginhodeveloper@gmail.com>
2021-07-24 12:44:22 -0300

    feat: add content to the home page and style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 
+import styles from "./home.module.scss";
+
 export default function Home() {
   return (
     <>
@@ -7,7 +9,20 @@ export default function Home() {
         <title>Início | ig.news</title>
       </Head>
 
-      <h1>Hello Next</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   );
 }
