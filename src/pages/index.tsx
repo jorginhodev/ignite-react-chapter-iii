0e751de9bcbbfc71ff1feeadacaf04fda92a921a
Jorge Ramos <jorginhodeveloper@gmail.com>
2021-07-24 15:36:01 -0300

    feat: change home page to SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import Image from "next/image";
 
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
@@ -33,13 +34,18 @@ export default function Home({ product }: HomeProps) {
           <SubscribeButton priceId={product.priceId} />
         </section>
 
-        <img src="/images/avatar.svg" alt="Girl Coding" />
+        <Image
+          src="/images/avatar.svg"
+          width="336"
+          height="521"
+          alt="ig.news seu blog de tecnologia"
+        />
       </main>
     </>
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1JGoggEchcSGZ1TkDxojXfUd");
 
   const product = {
@@ -54,5 +60,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // hours
   };
 };
